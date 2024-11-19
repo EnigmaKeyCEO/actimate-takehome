6ae5e99b057d90992c4858a120281b98d99ed353
DevCoup® <su@devcoup.com>
2024-11-19 04:00:24 -0500

    vite resolve @expo/vector-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-import path from 'path';
-import { PluginItem } from '@babel/core';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import path from "path";
+import { PluginItem } from "@babel/core";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
@@ -10,44 +10,73 @@ export default defineConfig(({ mode }) => ({
     react({
       babel: {
         plugins: [
-          mode === 'development' && ['react-refresh/babel', {}, 'react-refresh'],
+          mode === "development" && [
+            "react-refresh/babel",
+            {},
+            "react-refresh",
+          ],
         ].filter(Boolean) as PluginItem[],
-      }
-    })
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '#': path.resolve(__dirname, 'netlify/functions'),
-      'react-native': 'react-native-web',
-      '@react-native/assets-registry/registry': path.resolve(__dirname, 'node_modules/react-native-web/dist/modules/AssetRegistry/index.js'),
-      'react-native/Libraries/Utilities/codegenNativeComponent': path.resolve(__dirname, 'node_modules/react-native-web/dist/vendor/react-native/NativeComponent/index.js'),
+      "@": path.resolve(__dirname, "src"),
+      "#": path.resolve(__dirname, "netlify/functions"),
+      "react-native": "react-native-web",
+      "@react-native/assets-registry/registry": path.resolve(
+        __dirname,
+        "node_modules/react-native-web/dist/modules/AssetRegistry/index.js"
+      ),
+      "@expo/vector-icons": "@expo/vector-icons/dist/index.js",
+      "react-native/Libraries/Utilities/codegenNativeComponent": path.resolve(
+        __dirname,
+        "node_modules/react-native-web/dist/vendor/react-native/NativeComponent/index.js"
+      ),
     },
-    extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js']
+    extensions: [
+      ".web.tsx",
+      ".web.ts",
+      ".web.jsx",
+      ".web.js",
+      ".tsx",
+      ".ts",
+      ".jsx",
+      ".js",
+    ],
   },
   define: {
-    'process.env': {},
-    __DEV__: mode === 'development',
-    global: 'window',
+    "process.env": {},
+    __DEV__: mode === "development",
+    global: "window",
   },
   optimizeDeps: {
     include: [
-      'react-native-web',
-      '@expo/vector-icons',
-      'react-native-safe-area-context',
-      'expo-asset'
+      "react-native-web",
+      "@expo/vector-icons",
+      "react-native-safe-area-context",
+      "expo-asset",
     ],
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
-      resolveExtensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js'],
-      jsx: 'automatic',
-    }
+      resolveExtensions: [
+        ".web.tsx",
+        ".web.ts",
+        ".web.jsx",
+        ".web.js",
+        ".tsx",
+        ".ts",
+        ".jsx",
+        ".js",
+      ],
+      jsx: "automatic",
+    },
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  }
+    },
+  },
 }));
