549edeea3972ba5bf6ce741e0d38222dc40a2af9
DevCoup® <su@devcoup.com>
2024-11-19 09:26:31 -0500

    Fix vite (not the prettiest, but it works for now).

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, transformWithEsbuild } from "vite";
+import reactNativeWeb from "vite-plugin-react-native-web";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 import path from "path";
 import { PluginItem } from "@babel/core";
 
+const exclusionRegex = /node_modules(?!\/(@?expo[-/]vector-icons|lodash\..*|native-base|normalize-css-color|inline-style-prefixer))/;
+
 export default defineConfig(({ mode }) => ({
   plugins: [
+    {
+      name: "treat-js-files-as-jsx",
+      async transform(code, id) {
+        if (!id.match(exclusionRegex)) return null;
+
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic",
+        });
+      },
+    },
     viteCommonjs(),
-    react({
+    reactNativeWeb({
       babel: {
         plugins: [
           mode === "development" && [
@@ -27,13 +40,34 @@ export default defineConfig(({ mode }) => ({
         __dirname,
         "node_modules/react-native-web/dist/modules/AssetRegistry/index.js"
       ),
+      // "@expo/vector-icons": path.resolve(
+      //   __dirname,
+      //   "node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons"
+      // ),
       "@expo/vector-icons": path.resolve(
         __dirname,
-        "node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons"
+        "node_modules/@expo/vector-icons"
+      ),
+      "native-base": path.resolve(
+        __dirname,
+        "node_modules/native-base/lib/commonjs/index.js"
       ),
       "react-native/Libraries/Utilities/codegenNativeComponent": path.resolve(
         __dirname,
         "node_modules/react-native-web/dist/vendor/react-native/NativeComponent/index.js"
+      ),
+      "@react-native/normalize-colors": "normalize-css-color",
+      "inline-style-prefixer/lib/plugins/backgroundClip": path.resolve(
+        __dirname,
+        "node_modules/inline-style-prefixer/lib/plugins/backgroundClip"
+      ),
+      "inline-style-prefixer/lib/plugins/crossFade": path.resolve(
+        __dirname,
+        "node_modules/inline-style-prefixer/lib/plugins/crossFade"
+      ),
+      "inline-style-prefixer/lib/plugins/cursor": path.resolve(
+        __dirname,
+        "node_modules/inline-style-prefixer/lib/plugins/cursor"
       ),
     },
     extensions: [
@@ -58,6 +92,23 @@ export default defineConfig(({ mode }) => ({
       "@expo/vector-icons",
       "react-native-safe-area-context",
       "expo-asset",
+      "react",
+      "react/jsx-runtime",
+      "use-sync-external-store",
+      "use-sync-external-store/shim",
+      "lodash.get",
+      "lodash.isempty",
+      "lodash.omitby",
+      "tinycolor2",
+      "inline-style-prefixer/lib/plugins/backgroundClip",
+      "inline-style-prefixer/lib/plugins/crossFade",
+      "inline-style-prefixer/lib/plugins/cursor",
+      "@react-native/normalize-colors",
+      "inline-style-prefixer/lib/plugins/filter",
+      "inline-style-prefixer/lib/plugins/imageSet",
+      "inline-style-prefixer/lib/plugins/logical",
+      "postcss-value-parser",
+      "inline-style-prefixer/lib/createPrefixer",
     ],
     esbuildOptions: {
       loader: {
@@ -78,15 +129,65 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     commonjsOptions: {
+      include: [
+        /node_modules/,
+        /inline-style-prefixer/,
+        /postcss-value-parser/,
+        /lodash\..*/,
+      ],
+      requireReturnsDefault: "auto",
+      defaultIsModuleExports: true,
       transformMixedEsModules: true,
     },
     rollupOptions: {
-      external: ['react-native'],
+      external: [
+        "react-native",
+        "react",
+        "react/jsx-runtime",
+        "react-dom",
+        "use-sync-external-store",
+        "use-sync-external-store/shim",
+        "lodash.get",
+        "lodash.isempty",
+        "lodash.omitBy",
+        "tinycolor2",
+        "inline-style-prefixer/lib/plugins/backgroundClip",
+        "inline-style-prefixer/lib/plugins/crossFade",
+        "inline-style-prefixer/lib/plugins/cursor",
+        "inline-style-prefixer/lib/plugins/filter",
+        "inline-style-prefixer/lib/plugins/imageSet",
+        "inline-style-prefixer/lib/plugins/logical",
+        "inline-style-prefixer/lib/plugins/position",
+        "inline-style-prefixer/lib/plugins/sizing",
+        "inline-style-prefixer/lib/plugins/transition",
+        "inline-style-prefixer/lib/createPrefixer",
+        "postcss-value-parser",
+      ],
       output: {
         globals: {
-          'react-native': 'ReactNative'
-        }
-      }
-    }
+          "react-native": "ReactNative",
+          react: "React",
+          "react/jsx-runtime": "JSX",
+          "react-dom": "ReactDOM",
+          "use-sync-external-store": "useSyncExternalStore",
+          "use-sync-external-store/shim": "useSyncExternalStoreShim",
+          "lodash.get": "get",
+          "lodash.isempty": "isEmpty",
+          "lodash.omitby": "omitBy",
+          tinycolor2: "tinycolor",
+          "inline-style-prefixer/lib/plugins/backgroundClip": "backgroundClip",
+          "inline-style-prefixer/lib/plugins/crossFade": "crossFade",
+          "inline-style-prefixer/lib/plugins/cursor": "cursor",
+          "inline-style-prefixer/lib/plugins/filter": "filter",
+          "inline-style-prefixer/lib/plugins/imageSet": "imageSet",
+          "inline-style-prefixer/lib/plugins/logical": "logical",
+          "inline-style-prefixer/lib/plugins/position": "position",
+          "inline-style-prefixer/lib/plugins/sizing": "sizing",
+          "inline-style-prefixer/lib/plugins/transition": "transition",
+          "inline-style-prefixer/lib/createPrefixer": "createPrefixer",
+          "postcss-value-parser": "valueParser",
+        },
+      },
+    },
   },
 }));
