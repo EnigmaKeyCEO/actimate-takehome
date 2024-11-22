e441cf17a1e71c351c86bcb6ab27101ff711cf76
DevCoup® <su@devcoup.com>
2024-11-22 00:26:45 -0500

    fix EAS error related to metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,31 +1,33 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
-const path = require('path');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { withExpo } = require("@expo/webpack-config");
+const path = require("path");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    assetExts: [...defaultConfig.resolver.assetExts],
-    sourceExts: [...defaultConfig.resolver.sourceExts],
-    alias: {
-      '#': path.resolve(__dirname, './src')
-    }
-  },
-  transformer: {
-    ...defaultConfig.transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    assetPlugins: ['expo-asset/tools/hashAssetFiles']
-  },
-  server: {
-    enhanceMiddleware: (middleware) => {
-      return (req, res, next) => {
-        if (req.url.endsWith('.png')) {
-          req.headers['Content-Type'] = 'image/png';
-        }
-        return middleware(req, res, next);
-      };
-    }
-  }
-};
+module.exports = withExpo(
+  mergeConfig(defaultConfig, {
+    resolver: {
+      ...defaultConfig.resolver,
+      assetExts: [...defaultConfig.resolver.assetExts],
+      sourceExts: [...defaultConfig.resolver.sourceExts],
+      alias: {
+        "#": path.resolve(__dirname, "./src"),
+      },
+    },
+    transformer: {
+      ...defaultConfig.transformer,
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+    },
+    server: {
+      enhanceMiddleware: (middleware) => {
+        return (req, res, next) => {
+          if (req.url.endsWith(".png")) {
+            req.headers["Content-Type"] = "image/png";
+          }
+          return middleware(req, res, next);
+        };
+      },
+    },
+  })
+);
