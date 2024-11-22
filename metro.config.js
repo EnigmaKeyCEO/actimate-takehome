09ab5ed1a012a61f240fe0dcddd65145a101578f
DevCoup® <su@devcoup.com>
2024-11-22 02:28:47 -0500

    SUPER confusing... had to RTFM

--- metro.config.js
+++ metro.config.js
@@ -1,31 +1,10 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
-const path = require('path');
+// Learn more https://docs.expo.io/guides/customizing-metro
+/************/
+// MOST CONFUSING THING YET.... 
+// install '@expo/metro-config' but require it without the '@' symbol?!?!
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    assetExts: [...defaultConfig.resolver.assetExts],
-    sourceExts: [...defaultConfig.resolver.sourceExts],
-    alias: {
-      '#': path.resolve(__dirname, './src')
-    }
-  },
-  transformer: {
-    ...defaultConfig.transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    assetPlugins: ['expo-asset/tools/hashAssetFiles']
-  },
-  server: {
-    enhanceMiddleware: (middleware) => {
-      return (req, res, next) => {
-        if (req.url.endsWith('.png')) {
-          req.headers['Content-Type'] = 'image/png';
-        }
-        return middleware(req, res, next);
-      };
-    }
-  }
-};
+module.exports = config;
