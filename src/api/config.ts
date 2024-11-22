95dda959cd1378efa0f7b2d203f582cb0ce6d8e8
DevCoup® <su@devcoup.com>
2024-11-21 23:52:02 -0500

    fix the BaseURL thing properly. wrong KEY.

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  process.env.API_BASE_URL || "https://actimate-takehome.netlify.app/api";
+  process.env.VITE_API_BASE_URL || "https://actimate-takehome.netlify.app/api";
